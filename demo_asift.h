e76d23a8738d5d8b1b73bda0a165b3c95d8b0bc1
Mateus Azis <mateusazis@gmail.com>
2014-09-27 11:03:16 -0300

    Libpng dependentless.

--- demo_asift.h
+++ demo_asift.h
@@ -4,4 +4,4 @@
 #include <vector>
 #include <utility>
 
-std::vector<std::pair<Eigen::Vector2f, Eigen::Vector2f>> computeMyASIFT(const char* pngA, const char* pngB);
+std::vector<std::pair<Eigen::Vector2f, Eigen::Vector2f>> computeMyASIFT(std::vector<float> ipixels1, std::vector<float> ipixels2, int w1, int h1, int w2, int h2);
