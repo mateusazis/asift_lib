af29d1becd075d301a5bc966fd28f0bb4be6fb03
Mateus Azis <mateusazis@gmail.com>
2014-09-28 08:12:26 -0300

    Shared lib.

--- demo_asift.h
+++ demo_asift.h
@@ -4,4 +4,10 @@
 #include <vector>
 #include <utility>
 
-std::vector<std::pair<Eigen::Vector2f, Eigen::Vector2f>> computeMyASIFT(std::vector<float> ipixels1, std::vector<float> ipixels2, int w1, int h1, int w2, int h2);
+#ifdef ASIFTDLL_EXPORTS
+#define ASIFTDLL_API __declspec(dllexport) 
+#else
+#define ASIFTDLL_API __declspec(dllimport) 
+#endif
+
+ASIFTDLL_API std::vector<std::pair<Eigen::Vector2f, Eigen::Vector2f>> computeMyASIFT(std::vector<float> ipixels1, std::vector<float> ipixels2, int w1, int h1, int w2, int h2);
